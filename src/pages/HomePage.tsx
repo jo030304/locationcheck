d276d3be1039e20b986424692eb0303f2d88c190
jo030304 <j01040270138@gmail.com>
2025-07-07 17:32:39 +0900

    산책멍소

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,8 +3,6 @@ import KakaoMap from './KakaoMap';
 import BottomSheet from './BottomSheet';
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="w-full min-h-screen bg-[#EBFFEB] flex justify-center">
       <div className="w-full max-w-[430px] h-screen bg-white px-6 py-10 text-gray-800 text-xl">
