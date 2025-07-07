a378ebea743c98bb58a54bd1d4c0f7c19ec149be
jo030304 <j01040270138@gmail.com>
2025-07-07 22:39:52 +0900

    산책 멍소

--- src/pages/Function.tsx
+++ src/pages/Function.tsx
@@ -1,7 +1,7 @@
 const Function = () => {
   return (
-    <div className="border border-gray-300 rounded-2xl bg-white w-screen h-[10vh]">
-      안녕??
+    <div className="border border-gray-300 rounded-2xl bg-white w-screen h-[15vh]">
+
     </div>
   );
 }
--- src/pages/Walk_new.tsx
+++ src/pages/Walk_new.tsx
@@ -4,21 +4,15 @@ import Function from "./function";
 
 const Walk_new = () => {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      {/* 스크롤 가능한 영역 */}
-      <div className="w-full h-full overflow-y-auto pb-[80px]"> {/* ⬅︎ 여유 공간 확보 */}
-        <KakaoMap>
-          <Record />
-        </KakaoMap>
-      </div>
-
-      {/* 바닥 고정된 Function */}
-      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-[430px] px-4 z-50 flex justify-center">
-        <Function />
-      </div>
-
+    <div>
+      <KakaoMap>
+        <Record />
+        <div className="absolute bottom-0 w-full flex justify-center">
+          <Function />
+        </div>
+      </KakaoMap>
     </div>
   );
-};
+}
 
-export default Walk_new;
+export default Walk_new
