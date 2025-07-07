b7d5f963103587b0ab053379c1296502446f26a2
jo030304 <j01040270138@gmail.com>
2025-07-07 23:25:32 +0900

    산책 멍소

--- src/pages/Operator.tsx
+++ src/pages/Operator.tsx
@@ -1,9 +1,22 @@
+import { BsStopCircleFill } from "react-icons/bs";
+import { CgPlayPauseO } from "react-icons/cg";
+import { MdOutlineWaterDrop } from "react-icons/md";
+
 const Operator = () => {
   return (
-    <div className="border border-gray-300 rounded-2xl bg-white w-screen h-[15vh]">
-      안녕?
+    <div className="border border-gray-300 rounded-2xl bg-white w-screen h-[15vh] flex justify-around items-center px-4">
+      <button className="flex-1 flex justify-center">
+        <CgPlayPauseO className="w-[7vh] h-[7vh] max-w-[70px] max-h-[70px]" />
+      </button>
+      <button className="flex-1 flex justify-center">
+        <BsStopCircleFill className="w-[7vh] h-[7vh] max-w-[70px] max-h-[70px]" />
+      </button>
+      <button className="flex-1 flex justify-center">
+        <MdOutlineWaterDrop className="w-[7vh] h-[7vh] max-w-[70px] max-h-[70px]" />
+      </button>
     </div>
   );
 }
+
 
 export default Operator;
--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -1,6 +1,6 @@
 const Record = () => {
   return (
-    <div className="mx-7 border border-gray-300 rounded-4xl text-center p-4 bg-[#4FA65B] text-white 
+    <div className="mx-7 border border-gray-300 rounded-4xl text-center p-3 bg-[#4FA65B] text-white 
                     text-base sm:text-lg md:text-xl lg:text-2xl">
       산책 코스가 기록되고 있어요!
     </div>
